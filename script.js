ce07ec7613fcd2671e90187bdf6da3f20d3588d5
TestesGD <santosgabrie4591@gmail.com>
2025-10-18 18:59:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+    function openMenu() {
+      document.getElementById("side-menu").style.width = "250px";
+    }
+    function closeMenu() {
+      document.getElementById("side-menu").style.width = "0";
+    }
+
+    
